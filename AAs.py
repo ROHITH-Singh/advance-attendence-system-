7782da5c752d3b3d698f38bdd6482413536a9d69
ROHITH-Singh <rs9113848@gmail.com>
2021-12-20 00:51:07 +0530

    gi

--- AAs.py
+++ AAs.py
@@ -3,7 +3,7 @@ import numpy as np
 import face_recognition
 
 import os 
-path = '/home/rohitsingh/advance-attendence-system-/Saved Pictures'
+path = 'C:/Users/ROHIT SINGH/PycharmProjects/advance-attendence-system-/Saved Pictures'
 
 images = []
 classUsn = []
@@ -31,13 +31,27 @@ cap = cv2.VideoCapture(0)
 
 while True:
   success,img = cap.read()
-  imgsize = cv2.resize(img,(0,0),None,0.25,0.25)
-  imgsize = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
- 
-  facesCurr = face_recognition.face_locations(imgsize)[0]
-  encode = face_recognition.face_encodings(imgsize,facesCurr)[0]
+  imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+  imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+  facesCurrframe = face_recognition.face_locations(imgS)
+  encodeCurrframe = face_recognition.face_encodings(imgS, facesCurrframe)
 
-  for encodeface,faceLoc in zip(encode,facesCurr):
+
+
+  for encodeface,faceLoc in zip(encodeCurrframe,facesCurrframe):
     matches = face_recognition.compare_faces(encodeListstudents,encodeface)
     faceDis = face_recognition.face_distance(encodeListstudents,encodeface)
     print(faceDis)
+    matchIndex = np.argmin(faceDis)
+
+    if matches[matchIndex]:
+      Usn = classUsn[matchIndex]
+      print(classUsn)
+      y1,x2,y2,x1 = faceLoc
+      y1, x2, y2, x1 =   y1*4 ,x2*4 ,y2*4,x1*4
+      cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+      cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+      cv2.putText(img,Usn,(x1+15,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+  cv2.imshow('webcam',img)
+  cv2.waitKey(1)
